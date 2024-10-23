048c12ad868ffff9e1a57a046c96d5c047600570
HP <g00433839@atu.ie>
2024-10-23 15:22:44 +0100

    Base code

--- src/main/java/Lab6.java
+++ src/main/java/Lab6.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Lab6 {
     public static void main(String[] args) {
+
+        System.out.println("Please enter in a number between 0 and 9");
+        Scanner myscan = new Scanner(System.in);
+
+
+        String input = myscan.nextLine();
+
+        int Mynum = Integer.parseInt(input);
+
+        if(Mynum >=0 && Mynum <= 9) {
+            System.out.println("The number is " + Mynum);
+        }else{
+            System.out.println("Not a valid number");
+        }
+
 
     }
 }
